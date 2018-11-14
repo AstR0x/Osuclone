eeafa429e110e3cb95007153427b3fc5ce1ae944
AstR0x <vesel4ack98@mail.ru>
2018-11-15 00:48:49 +0300

    added clickCircle function

--- js/script.js
+++ js/script.js
@@ -16,10 +16,12 @@ $(document).ready(function () {
 
     function getCircle() {
       let amountOfCircle = getRandomNumber(3, 6);
+      var randomArray = getRandomArray(amountOfCircle);
       let numberOfCircle;
       for(var i = 1; i <= amountOfCircle; i++) {
-        numberOfCircle = getRandomNumber(1, 55);
-        gridChild.eq(numberOfCircle)
+        // numberOfCircle = getRandomNumber(1, 55);
+
+        gridChild.eq(randomArray[i - 1])
           .css({'visibility':'visible',
             'background-color':`rgb(
              ${getRandomNumber(0,200)},
@@ -31,29 +33,42 @@ $(document).ready(function () {
       }
     }
 
-    function clickCircle() {
-      let circle = $('.circle');
-      circle.each(function(index, elem) {
-          mass[index] = $(`#circleNumber${index + 1}`);
-      });
-
-      circle.each(function(index, elem) {
-        $(this).click(function() {
-           $(this).css( {'visibility':'hidden'} );
-           if(index !== 1) {
-             if(mass[index - 1].css('visibility') === 'hidden') {
-               score += 100;
-             } else {
-               score -= 100;
-             }
+     function clickCircle() {
+       let circle = $('.circle');
+       circle.each(function() {
+         var count = 0;
+         $(this).click(function() {
+           var id = ($(this).attr('id'));
+           id = id[id.length - 1];
+           for(var i = 1; i <= id; i++) {
+            $(`#circleNumber${i}`).fadeOut(150);
            }
-          console.log(score);
-        })
+
+
+         })
+
       })
     };
+
+    function getRandomArray(amount) {
+      let arrayOfRandomNumber = [];
+      let randomNumber;
+      arrayOfRandomNumber[0] = getRandomNumber(1, 55);
+      for(var i = 1; i < amount;) {
+        randomNumber = getRandomNumber(1, 55);
+        if(arrayOfRandomNumber.indexOf(randomNumber) !== -1) {
+          continue;
+        }
+        arrayOfRandomNumber[i] = randomNumber;
+        i++;
+      }
+      return arrayOfRandomNumber;
+    }
 
     function getRandomNumber(min, max) {
       return Math.round((Math.random() * (max - min) + min))
     }
+
+
   })
 });
