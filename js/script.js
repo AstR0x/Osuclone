280d6d312889f242f220a97f3ed781153adabd5e
AstR0x <vesel4ack98@mail.ru>
2018-11-21 00:12:28 +0300

    added sounds, permanent getCircle, new background, score block

--- js/script.js
+++ js/script.js
@@ -1,72 +1,106 @@
 $(document).ready(function () {
+
+
+    let clickSound1 = new Audio();
+    let clickSound2 = new Audio();
+    clickSound1.src = 'sounds/clickSound1.mp3';
+    clickSound2.src = 'sounds/clickSound2.mp3';
+
+
+
   let grid = $('#grid');
   let gridChild = $('#grid div');
-  var mass = [];
-  let scoreBlock = $('.score');
   let score = 0;
+  let scoreBlock = $('.score');
   const buttonStart = $('.button-start');
 
-  buttonStart.click(function() {
+  buttonStart.click(function () {
     buttonStart.fadeOut(150);
-    setTimeout(function(){
-      grid.css({'display':'grid'});
+    setTimeout(function () {
+      grid.css({'display': 'grid'});
       getCircle();
-      clickCircle();
+      scoreBlock.show();
     }, 150);
 
     function getCircle() {
       let amountOfCircle = getRandomNumber(3, 6);
-      var randomArray = getRandomArray(amountOfCircle);
-      let numberOfCircle;
-      for(var i = 1; i <= amountOfCircle; i++) {
-        // numberOfCircle = getRandomNumber(1, 55);
-        console.log(randomArray);
+      let randomArray = getRandomArray(amountOfCircle);
+      for (let i = 1; i <= amountOfCircle; i++) {
         gridChild.eq(randomArray[i - 1])
-          .css({'visibility':'visible',
-            'background-color':`rgb(
-             ${getRandomNumber(0,200)},
-             ${getRandomNumber(0,200)},
-             ${getRandomNumber(0,200)})`})
+          .css({
+            'visibility': 'visible',
+            'opacity': '1', 'font-size': '48px',
+            'padding-top': '14px',
+            'background-color': `rgb( 
+             ${getRandomNumber(0, 200)},
+             ${getRandomNumber(0, 200)},
+             ${getRandomNumber(0, 200)})`
+          })
           .text(i)
-          .attr('id', `circleNumber${i}`)
-          .addClass('circle')
+          .attr('id', `circleNumber${i}`);
+
+        if (i == amountOfCircle) {
+          gridChild.eq(randomArray[i - 1]).addClass('circle lastCircle');
+        }
+        else {
+          gridChild.eq(randomArray[i - 1]).addClass('circle');
+        }
+      }
+      if ($('.lastCircle').hasClass('circle')) {
+        clickCircle();
       }
     }
-
-     function clickCircle() {
-       let circle = $('.circle');
-       circle.each(function() {
-         var count = 0;
-         $(this).click(function() {
-           var id = ($(this).attr('id'));
-           id = id[id.length - 1];
-           for(var i = 1; i <= id; i++) {
-             if(i == id) {
-               $(`#circleNumber${i}`).text('\u2713').css( { 'font-size':'+=8px', 'padding-top':'-=10px' } );
-             } else {
-               $(`#circleNumber${i}`).text('\u274c').css( { 'background-color':'#ff3333','font-size':'+=8px', 'padding-top':'-=10px'} );
-             }
-             $(`#circleNumber${i}`).animate( { 'opacity':'0', 'visiblity':'hidden' }, 300);
-           }
-           score += 100;
-           console.log(score);
-         });
-
+    function clickCircle() {
+      let circle = $('.circle');
+      circle.each(function () {
+        let circleNumber;
+        $(this).click(function () {
+          let id = ($(this).attr('id'));
+          id = id[id.length - 1];
+          for (let i = 1; i <= id; i++) {
+            circleNumber = $(`#circleNumber${i}`);
+            if (i == id) {
+              circleNumber.text('\u2713').css({
+                'font-size': '48px',
+                'padding-top': '8px'
+              });
+            } else {
+              circleNumber
+                .text('\u274c')
+                .css({
+                  'background-color': '#ff3333',
+                  'font-size': '48px',
+                  'padding-top': '8px'
+                });
+            }
+            circleNumber.animate({'opacity': '0', 'visiblity': 'hidden'}, 300)
+              .removeClass('circle')
+              .removeAttr('id');
+            if (!$('.lastCircle').hasClass('circle')) {
+              $('.lastCircle').removeClass('lastCircle');
+              getCircle();
+            }
+          }
+          score += 100;
+          scoreBlock.text(score);
+          getSound();
+        });
       })
     };
 
     function getRandomArray(amount) {
       let arrayOfRandomNumber = [];
       let randomNumber;
-      arrayOfRandomNumber[0] = getRandomNumber(1, 55);
-      for(var i = 1; i < amount;) {
-        randomNumber = getRandomNumber(1, 55);
-        if(arrayOfRandomNumber.indexOf(randomNumber) !== -1) {
+      arrayOfRandomNumber[0] = getRandomNumber(1, 54);
+      for (let i = 1; i < amount;) {
+        randomNumber = getRandomNumber(1, 54);
+        if (arrayOfRandomNumber.indexOf(randomNumber) !== -1) {
           continue;
         }
         arrayOfRandomNumber[i] = randomNumber;
         i++;
       }
+      console.log(arrayOfRandomNumber);
       return arrayOfRandomNumber;
     }
 
@@ -74,6 +108,12 @@ $(document).ready(function () {
       return Math.round((Math.random() * (max - min) + min))
     }
 
-
-  })
+    function getSound() {
+      if (getRandomNumber(0, 1)) {
+        clickSound1.play();
+      } else {
+        clickSound2.play();
+      }
+    }
+  });
 });
