e79dd551aa27efb24130c378bf92c5472f514ce5
Vlad Gumerov <astr0xy1@gmail.com>
2018-11-23 00:09:27 +0300

    added comments and fixed bugs

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
   clickSound4.src = 'sounds/clickSound4.mp3';
   clickSound5.src = 'sounds/clickSound5.mp3';
 
-  var sounds = [clickSound1, clickSound2, clickSound3, clickSound4, clickSound5];
+  let sounds = [clickSound1, clickSound2, clickSound3, clickSound4, clickSound5];
 
   let point = 0;
   const tryAgainBtn = $('.try-again-button');
@@ -26,27 +26,36 @@ $(document).ready(function () {
   const score = $('.score');
   const scoreBlock = $('.score-block');
   const timeBlock = $('.time-block');
-  const buttonStart = $('.button-start');
+  const buttonStart = $('.start-button');
   const finishScore = $('.finish-score');
 
+  let lastNumberOfArray;
+
+  //Запуск игры по нажатию на 'start'
   buttonStart.click(function () {
-    let time = performance.now();
+    grid.css({'display': 'grid'});
+    let time = performance.now(); //Получаем начальное время
     buttonStart.fadeOut(150);
     setTimeout(function () {
       music.play();
-      grid.css({'display': 'grid'});
       getCircle();
       scoreBlock.show();
       timeBlock.show();
-      setInterval(getTime, 1000);
+      setInterval(getTime, 1000); //Обновление времени каждую секунду
     }, 150);
 
     function getCircle() {
       let amountOfCircle = getRandomNumber(3, 6);
-      let randomArray = getRandomArray(amountOfCircle);
+      let randomArray = getRandomArray(amountOfCircle, lastNumberOfArray);
+
+      lastNumberOfArray = randomArray[randomArray.length - 1];
+
+      console.log(randomArray);
+      console.log(lastNumberOfArray);
+
       for (let i = 1; i <= amountOfCircle; i++) {
-        gridChild.eq(randomArray[i - 1])
-          .css({'visibility':'visible', 'opacity':'1', 'padding-top':'12px', 'background-color': `rgb( 
+        gridChild.eq(randomArray[i - 1]).css({'visibility':'visible', 'opacity':'1', //Появление кружков
+          'padding-top':'12px', 'background-color': `rgb( 
              ${getRandomNumber(0, 200)},
              ${getRandomNumber(0, 200)},
              ${getRandomNumber(0, 200)})`
@@ -55,8 +64,8 @@ $(document).ready(function () {
           .attr('id', `circleNumber${i}`);
 
         if (i == amountOfCircle) {
-          gridChild.eq(randomArray[i - 1]).addClass('circle lastCircle');
-        }
+          gridChild.eq(randomArray[i - 1]).addClass('circle lastCircle'); //Последний кружок из группы получает
+        }                                                                 //класс lastCircle
         else {
           gridChild.eq(randomArray[i - 1]).addClass('circle');
         }
@@ -71,22 +80,27 @@ $(document).ready(function () {
       circle.each(function () {
         let circleNumber;
         $(this).click(function () {
-          let id = ($(this).attr('id'));
-          id = id[12];
-          for (let i = 1; i <= id; i++) {
-            circleNumber = $(`#circleNumber${i}`);
-            if (i == id) {
+
+          let id = ($(this).attr('id'));       //Получаем номер из
+          let number = id[id.length - 1];      // класса кружочка
+
+          for (let i = 1; i <= number; i++) {      //Обрабатываем кружочек,
+            circleNumber = $(`#circleNumber${i}`); //по которому нажали и все предшествующие
+
+            if (i == number) {
               circleNumber.text('\u2713').css({'padding-top':'6px'});
             } else {
               circleNumber
                 .text('\u274c')
                 .css({'background-color': '#ff3333', 'padding-top':'6px'});
             }
-            circleNumber.animate({'opacity': '0', 'visiblity': 'hidden'}, 300)
+
+            circleNumber.animate({'opacity': '0', 'visiblity': 'hidden'}, 300) //Скрываем кружочки
               .removeClass('circle')
               .removeAttr('id');
-            if (!$('.lastCircle').hasClass('circle')) {
-              $('.lastCircle').removeClass('lastCircle');
+
+            if (!$('.lastCircle').hasClass('circle')) {   //Если последний кружочек уже был обработан
+              $('.lastCircle').removeClass('lastCircle'); //то удаляем класс и генерируем новый набор кружков
               getCircle();
             }
           }
@@ -97,20 +111,20 @@ $(document).ready(function () {
       })
     };
 
-    function getRandomArray(amount) {
-      let arrayOfRandomNumber = [];
+    function getRandomArray(amount, lastNumberOfArray) {
+      let arrayOfRandomUniqueNumber = [];
       let randomNumber;
-      arrayOfRandomNumber[0] = getRandomNumber(0, 59);
+      arrayOfRandomUniqueNumber[0] = getRandomNumber(0, 59);
+
       for (let i = 1; i < amount;) {
         randomNumber = getRandomNumber(0, 59);
-        if (arrayOfRandomNumber.indexOf(randomNumber) !== -1) {
+        if (arrayOfRandomUniqueNumber.indexOf(randomNumber) !== -1 || randomNumber === lastNumberOfArray) {
           continue;
         }
-        arrayOfRandomNumber[i] = randomNumber;
+        arrayOfRandomUniqueNumber[i] = randomNumber;
         i++;
       }
-      console.log(arrayOfRandomNumber);
-      return arrayOfRandomNumber;
+      return arrayOfRandomUniqueNumber;
     }
 
     function getRandomNumber(min, max) {
@@ -123,7 +137,7 @@ $(document).ready(function () {
 
     function getTime() {
         clock.text(Math.round((performance.now() - time) / 1000));
-        if (parseInt(clock.text()) >= 60) {
+        if (parseInt(clock.text()) >= 30) {
           timeIsOver();
       }
     }
@@ -132,17 +146,19 @@ $(document).ready(function () {
       grid.fadeOut();
       timeBlock.fadeOut();
       scoreBlock.fadeOut();
+
       setTimeout(function() {
         finishScore.fadeIn().text(`Time is over!\nYour score: ${point}`);
       }, 1500);
+
       setTimeout(function() {
         tryAgainBtn.fadeIn();
       }, 4500);
+
       tryAgainBtn.click(function() {
         location.reload();
       });
     }
-
 
   });
 });
